0e98af0f9cab31b058e681a36aaa3e31d7ced9e5
AmitChigare <amit.gokakfalls17@gmail.com>
2023-08-16 23:39:16 +0530

    Added Product page

--- frontend-starter/src/App.js
+++ frontend-starter/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Header />
+        <main className="py-3">
+          <Router>
+            <Routes>
+              {/* <Route path="/" element={<HomeScreen />} exact /> */}
+              <Route path="/" Component={HomeScreen} exact />
+            </Routes>
+          </Router>
+        </main>
         <Footer />
       </div>
     </div>
